67ed908dd89fa1db11db3e753e8884aa3e174e0e
SaswithSahu <sahusaswith7@gmail.com>
2023-07-09 18:52:35 +0000

    solution

--- src/components/EachCategory/index.js
+++ src/components/EachCategory/index.js
@@ -7,12 +7,23 @@ const EachCategory = props => {
   }
   const className = activated ? 'activated' : 'list-item'
   return (
-    <button type="button" onClick={onSelectCategory}>
-      <li className={className}>
-        <h1>{eachItem}</h1>
-        <p>Unlimited Jokes On {eachItem}</p>
-      </li>
-    </button>
+    <>
+      <div className="desktop">
+        <button type="button" onClick={onSelectCategory}>
+          <li className={className}>
+            <h1>{eachItem}</h1>
+            <p>Unlimited Jokes On {eachItem}</p>
+          </li>
+        </button>
+      </div>
+      <div className="mobile">
+        <button type="button" onClick={onSelectCategory}>
+          <li className="list-item">
+            <h1>{eachItem}</h1>
+          </li>
+        </button>
+      </div>
+    </>
   )
 }
 
